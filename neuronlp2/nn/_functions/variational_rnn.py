ae4eda107390df40fda6bf48b65dc2d6c4665e67
Max Ma <xuezhe.ma@gmail.com>
2017-10-31 17:41:45 -0400

    variational dropout for rnn

--- neuronlp2/nn/_functions/variational_rnn.py
+++ neuronlp2/nn/_functions/variational_rnn.py
@@ -1,6 +1,7 @@
 __author__ = 'max'
 
 import torch
+from torch.nn._functions.thnn import rnnFusedPointwise as fusedBackend
 from torch.nn import functional as F
 
 
@@ -15,4 +16,50 @@ def VarRNNTanhCell(input, hidden, w_ih, w_hh, b_ih=None, b_hh=None, noise=None):
     if noise is not None:
         hidden = hidden * noise
     hy = F.tanh(F.linear(input, w_ih, b_ih) + F.linear(hidden, w_hh, b_hh))
+    return hy
+
+
+def VarLSTMCell(input, hidden, w_ih, w_hh, b_ih=None, b_hh=None, noise=None):
+    # if input.is_cuda:
+    #     igates = F.linear(input, w_ih)
+    #     hgates = F.linear(hidden[0], w_hh)
+    #     state = fusedBackend.LSTMFused.apply
+    #     return state(igates, hgates, hidden[1]) if b_ih is None else state(igates, hgates, hidden[1], b_ih, b_hh)
+
+    hx, cx = hidden
+    if noise is not None:
+        hx = hx * noise
+    gates = F.linear(input, w_ih, b_ih) + F.linear(hx, w_hh, b_hh)
+
+    ingate, forgetgate, cellgate, outgate = gates.chunk(4, 1)
+
+    ingate = F.sigmoid(ingate)
+    forgetgate = F.sigmoid(forgetgate)
+    cellgate = F.tanh(cellgate)
+    outgate = F.sigmoid(outgate)
+
+    cy = (forgetgate * cx) + (ingate * cellgate)
+    hy = outgate * F.tanh(cy)
+
+    return hy, cy
+
+
+def VarGRUCell(input, hidden, w_ih, w_hh, b_ih=None, b_hh=None, noise=None):
+    # if input.is_cuda:
+    #     gi = F.linear(input, w_ih)
+    #     gh = F.linear(hidden, w_hh)
+    #     state = fusedBackend.GRUFused.apply
+    #     return state(gi, gh, hidden) if b_ih is None else state(gi, gh, hidden, b_ih, b_hh)
+
+    hx = hidden if noise is None else hidden * noise
+    gi = F.linear(input, w_ih, b_ih)
+    gh = F.linear(hx, w_hh, b_hh)
+    i_r, i_i, i_n = gi.chunk(3, 1)
+    h_r, h_i, h_n = gh.chunk(3, 1)
+
+    resetgate = F.sigmoid(i_r + h_r)
+    inputgate = F.sigmoid(i_i + h_i)
+    newgate = F.tanh(i_n + resetgate * h_n)
+    hy = newgate + inputgate * (hidden - newgate)
+
     return hy
--- neuronlp2/nn/modules/variational_rnn.py
+++ neuronlp2/nn/modules/variational_rnn.py
@@ -5,8 +5,7 @@ import torch
 import torch.nn as nn
 from torch.nn.parameter import Parameter
 from torch.autograd import Variable
-import torch.nn.functional as F
-from .._functions import VarRNNReLUCell, VarRNNTanhCell
+import neuronlp2.nn._functions as rnnF
 
 
 class VarRNNCellBase(nn.Module):
@@ -95,14 +94,163 @@ class VarRNNCell(VarRNNCellBase):
 
     def forward(self, input, hx):
         if self.nonlinearity == "tanh":
-            func = VarRNNTanhCell
+            func = rnnF.VarRNNTanhCell
         elif self.nonlinearity == "relu":
-            func = VarRNNReLUCell
+            func = rnnF.VarRNNReLUCell
         else:
             raise RuntimeError(
                 "Unknown nonlinearity: {}".format(self.nonlinearity))
 
         return func(
+            input, hx,
+            self.weight_ih, self.weight_hh,
+            self.bias_ih, self.bias_hh,
+            self.noise,
+        )
+
+
+class VarLSTMCell(VarRNNCellBase):
+    """
+    A long short-term memory (LSTM) cell with variational dropout.
+
+    .. math::
+
+        \begin{array}{ll}
+        i = \mathrm{sigmoid}(W_{ii} x + b_{ii} + W_{hi} h + b_{hi}) \\
+        f = \mathrm{sigmoid}(W_{if} x + b_{if} + W_{hf} h + b_{hf}) \\
+        g = \tanh(W_{ig} x + b_{ig} + W_{hc} h + b_{hg}) \\
+        o = \mathrm{sigmoid}(W_{io} x + b_{io} + W_{ho} h + b_{ho}) \\
+        c' = f * c + i * g \\
+        h' = o * \tanh(c') \\
+        \end{array}
+
+    Args:
+        input_size: The number of expected features in the input x
+        hidden_size: The number of features in the hidden state h
+        bias: If `False`, then the layer does not use bias weights `b_ih` and
+            `b_hh`. Default: True
+        p: (float, optional): the drop probability. Default: 0.5
+
+    Inputs: input, (h_0, c_0)
+        - **input** (batch, input_size): tensor containing input features
+        - **h_0** (batch, hidden_size): tensor containing the initial hidden
+          state for each element in the batch.
+        - **c_0** (batch. hidden_size): tensor containing the initial cell state
+          for each element in the batch.
+
+    Outputs: h_1, c_1
+        - **h_1** (batch, hidden_size): tensor containing the next hidden state
+          for each element in the batch
+        - **c_1** (batch, hidden_size): tensor containing the next cell state
+          for each element in the batch
+
+    Attributes:
+        weight_ih: the learnable input-hidden weights, of shape
+            `(4*hidden_size x input_size)`
+        weight_hh: the learnable hidden-hidden weights, of shape
+            `(4*hidden_size x hidden_size)`
+        bias_ih: the learnable input-hidden bias, of shape `(4*hidden_size)`
+        bias_hh: the learnable hidden-hidden bias, of shape `(4*hidden_size)`
+    """
+
+    def __init__(self, input_size, hidden_size, bias=True):
+        super(VarLSTMCell, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.bias = bias
+        self.weight_ih = Parameter(torch.Tensor(4 * hidden_size, input_size))
+        self.weight_hh = Parameter(torch.Tensor(4 * hidden_size, hidden_size))
+        if bias:
+            self.bias_ih = Parameter(torch.Tensor(4 * hidden_size))
+            self.bias_hh = Parameter(torch.Tensor(4 * hidden_size))
+        else:
+            self.register_parameter('bias_ih', None)
+            self.register_parameter('bias_hh', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1.0 / math.sqrt(self.hidden_size)
+        for weight in self.parameters():
+            weight.data.uniform_(-stdv, stdv)
+
+    def reset_noise(self, batch_size):
+        if self.training and self.p:
+            noise = self.weight_hh.data.new(batch_size, self.hidden_size).bernoulli_(1.0 - self.p) / (1.0 - self.p)
+            self.noise = Variable(noise)
+
+    def forward(self, input, hx):
+        return rnnF.VarLSTMCell(
+            input, hx,
+            self.weight_ih, self.weight_hh,
+            self.bias_ih, self.bias_hh,
+            self.noise,
+        )
+
+
+class VarGRUCell(VarRNNCellBase):
+    """A gated recurrent unit (GRU) cell with variational dropout.
+
+    .. math::
+
+        \begin{array}{ll}
+        r = \mathrm{sigmoid}(W_{ir} x + b_{ir} + W_{hr} h + b_{hr}) \\
+        z = \mathrm{sigmoid}(W_{iz} x + b_{iz} + W_{hz} h + b_{hz}) \\
+        n = \tanh(W_{in} x + b_{in} + r * (W_{hn} h + b_{hn})) \\
+        h' = (1 - z) * n + z * h
+        \end{array}
+
+    Args:
+        input_size: The number of expected features in the input x
+        hidden_size: The number of features in the hidden state h
+        bias: If `False`, then the layer does not use bias weights `b_ih` and
+            `b_hh`. Default: `True`
+        p: (float, optional): the drop probability. Default: 0.5
+
+    Inputs: input, hidden
+        - **input** (batch, input_size): tensor containing input features
+        - **hidden** (batch, hidden_size): tensor containing the initial hidden
+          state for each element in the batch.
+
+    Outputs: h'
+        - **h'**: (batch, hidden_size): tensor containing the next hidden state
+          for each element in the batch
+
+    Attributes:
+        weight_ih: the learnable input-hidden weights, of shape
+            `(3*hidden_size x input_size)`
+        weight_hh: the learnable hidden-hidden weights, of shape
+            `(3*hidden_size x hidden_size)`
+        bias_ih: the learnable input-hidden bias, of shape `(3*hidden_size)`
+        bias_hh: the learnable hidden-hidden bias, of shape `(3*hidden_size)`
+    """
+
+    def __init__(self, input_size, hidden_size, bias=True):
+        super(VarGRUCell, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.bias = bias
+        self.weight_ih = Parameter(torch.Tensor(3 * hidden_size, input_size))
+        self.weight_hh = Parameter(torch.Tensor(3 * hidden_size, hidden_size))
+        if bias:
+            self.bias_ih = Parameter(torch.Tensor(3 * hidden_size))
+            self.bias_hh = Parameter(torch.Tensor(3 * hidden_size))
+        else:
+            self.register_parameter('bias_ih', None)
+            self.register_parameter('bias_hh', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1.0 / math.sqrt(self.hidden_size)
+        for weight in self.parameters():
+            weight.data.uniform_(-stdv, stdv)
+
+    def reset_noise(self, batch_size):
+        if self.training and self.p:
+            noise = self.weight_hh.data.new(batch_size, self.hidden_size).bernoulli_(1.0 - self.p) / (1.0 - self.p)
+            self.noise = Variable(noise)
+
+    def forward(self, input, hx):
+        return rnnF.VarGRUCell(
             input, hx,
             self.weight_ih, self.weight_hh,
             self.bias_ih, self.bias_hh,
