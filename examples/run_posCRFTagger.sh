3ac99436c099ea3f1562b2e391ad12f23bf2132a
Max Ma <xuezhe.ma@gmail.com>
2017-11-06 23:19:59 -0500

    runs

--- examples/run_posCRFTagger.sh
+++ examples/run_posCRFTagger.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 CUDA_VISIBLE_DEVICES=1 python examples/posCRFTagger.py --mode LSTM --num_epochs 200 --batch_size 16 --hidden_size 256 --num_filters 30 \
- --learning_rate 0.01 --decay_rate 0.05 --schedule 10 --gamma 0.0 --dropout std --p 0.5 \
+ --learning_rate 0.01 --decay_rate 0.05 --schedule 2 --gamma 0.0 --dropout std --p 0.5 \
  --train "data/POS-penn/wsj/split1/wsj1.train.original" --dev "data/POS-penn/wsj/split1/wsj1.dev.original" --test "data/POS-penn/wsj/split1/wsj1.test.original"
