5c2c65b6269a5aa6be217e6783ac125fcd2376e3
Max Ma <xuezhe.ma@gmail.com>
2017-11-07 17:00:27 -0500

    elu

--- neuronlp2/models/sequence_labeling.py
+++ neuronlp2/models/sequence_labeling.py
@@ -119,6 +119,95 @@ class BiRecurrentConv(nn.Module):
                    (torch.eq(preds, target).type_as(output)).sum(), preds
 
 
+class BiVarRecurrentConv(nn.Module):
+    def __init__(self, word_dim, num_words, char_dim, num_chars, num_filters, kernel_size,
+                 rnn_mode, hidden_size, num_layers, num_labels, tag_space=0,
+                 embedd_word=None, embedd_char=None, p_in=0.2, p_rnn=0.5):
+        super(BiVarRecurrentConv, self).__init__()
+
+        self.word_embedd = Embedding(num_words, word_dim, init_embedding=embedd_word)
+        self.char_embedd = Embedding(num_chars, char_dim, init_embedding=embedd_char)
+        self.conv1d = nn.Conv1d(char_dim, num_filters, kernel_size, padding=kernel_size - 1)
+        self.dropout_in = nn.Dropout2d(p=p_in)
+        self.dropout_rnn = nn.Dropout2d(p_rnn)
+
+        if rnn_mode == 'RNN':
+            RNN = VarMaskedRNN
+        elif rnn_mode == 'LSTM':
+            RNN = VarMaskedLSTM
+        elif rnn_mode == 'GRU':
+            RNN = VarMaskedGRU
+        else:
+            raise ValueError('Unknown RNN mode: %s' % rnn_mode)
+
+        self.rnn = RNN(word_dim + num_filters, hidden_size, num_layers=num_layers,
+                       batch_first=True, bidirectional=True, dropout=p_rnn)
+
+        self.dense_tag = None
+        out_dim = hidden_size * 2
+        if tag_space:
+            self.dense_tag = nn.Linear(out_dim, tag_space)
+            out_dim = tag_space
+        self.dense_softmax = nn.Linear(out_dim, num_labels)
+
+        self.logsoftmax = nn.LogSoftmax()
+        self.nll_loss = nn.NLLLoss(size_average=False)
+
+    def forward(self, input_word, input_char, mask=None, hx=None):
+        # [batch, length, word_dim]
+        word = self.word_embedd(input_word)
+
+        # [batch, length, char_length, char_dim]
+        char = self.char_embedd(input_char)
+        char_size = char.size()
+        # first transform to [batch *length, char_length, char_dim]
+        # then transpose to [batch * length, char_dim, char_length]
+        char = char.view(char_size[0] * char_size[1], char_size[2], char_size[3]).transpose(1, 2)
+        # put into cnn [batch*length, char_filters, char_length]
+        # then put into maxpooling [batch * length, char_filters]
+        char, _ = self.conv1d(char).max(dim=2)
+        # reshape to [batch, length, char_filters]
+        char = char.view(char_size[0], char_size[1], -1)
+
+        # concatenate word and char [batch, length, word_dim+char_filter]
+        input = F.tanh(torch.cat([word, char], dim=2))
+        # apply dropout
+        # [batch, length, dim] --> [batch, dim, length] --> [batch, length, dim]
+        input = self.dropout_in(input.transpose(1, 2)).transpose(1, 2)
+        # output from rnn [batch, length, hidden_size]
+        output, hn = self.rnn(input, mask, hx=hx)
+
+        output = self.dropout_rnn(output.transpose(1, 2)).transpose(1, 2)
+
+        if self.dense_tag is not None:
+            # [batch, length, tag_space]
+            output = F.elu(self.dense_tag(output))
+
+        return output, hn, mask
+
+    def loss(self, input_word, input_char, target, mask=None, hx=None, leading_symbolic=0):
+        # [batch, length, num_labels]
+        output, _, _ = self.forward(input_word, input_char, mask=mask, hx=hx)
+        # [batch, length, num_labels]
+        output = self.dense_softmax(output)
+        # preds = [batch, length]
+        _, preds = torch.max(output[:, :, leading_symbolic:], dim=2)
+        preds += leading_symbolic
+
+        output_size = output.size()
+        # [batch * length, num_labels]
+        output_size = (output_size[0] * output_size[1], output_size[2])
+        output = output.view(output_size)
+        if mask is not None:
+            return self.nll_loss(self.logsoftmax(output) * mask.view(output_size[0], 1),
+                                 target.view(-1)) / mask.sum(), \
+               (torch.eq(preds, target).type_as(mask) * mask).sum(), preds
+        else:
+            num = output_size[0] * output_size[1]
+            return self.nll_loss(self.logsoftmax(output), target.view(-1)) / num, \
+                   (torch.eq(preds, target).type_as(output)).sum(), preds
+
+
 class BiRecurrentConvCRF(nn.Module):
     def __init__(self, word_dim, num_words, char_dim, num_chars, num_filters, kernel_size,
                  rnn_mode, hidden_size, num_layers, num_labels, tag_space=0,
@@ -228,90 +317,3 @@ class BiRecurrentConvCRF(nn.Module):
         else:
             return preds, (torch.eq(preds, target.data).float() * mask.data).sum()
 
-class BiVarRecurrentConv(nn.Module):
-    def __init__(self, word_dim, num_words, char_dim, num_chars, num_filters, kernel_size,
-                 rnn_mode, hidden_size, num_layers, num_labels, tag_space=0,
-                 embedd_word=None, embedd_char=None, p_in=0.2, p_rnn=0.5):
-        super(BiVarRecurrentConv, self).__init__()
-
-        self.word_embedd = Embedding(num_words, word_dim, init_embedding=embedd_word)
-        self.char_embedd = Embedding(num_chars, char_dim, init_embedding=embedd_char)
-        self.conv1d = nn.Conv1d(char_dim, num_filters, kernel_size, padding=kernel_size - 1)
-        self.dropout_in = nn.Dropout2d(p=p_in)
-        self.dropout_rnn = nn.Dropout2d(p_rnn)
-
-        if rnn_mode == 'RNN':
-            RNN = VarMaskedRNN
-        elif rnn_mode == 'LSTM':
-            RNN = VarMaskedLSTM
-        elif rnn_mode == 'GRU':
-            RNN = VarMaskedGRU
-        else:
-            raise ValueError('Unknown RNN mode: %s' % rnn_mode)
-
-        self.rnn = RNN(word_dim + num_filters, hidden_size, num_layers=num_layers,
-                       batch_first=True, bidirectional=True, dropout=p_rnn)
-
-        self.dense_tag = None
-        out_dim = hidden_size * 2
-        if tag_space:
-            self.dense_tag = nn.Linear(out_dim, tag_space)
-            out_dim = tag_space
-        self.dense_softmax = nn.Linear(out_dim, num_labels)
-
-        self.logsoftmax = nn.LogSoftmax()
-        self.nll_loss = nn.NLLLoss(size_average=False)
-
-    def forward(self, input_word, input_char, mask=None, hx=None):
-        # [batch, length, word_dim]
-        word = self.word_embedd(input_word)
-
-        # [batch, length, char_length, char_dim]
-        char = self.char_embedd(input_char)
-        char_size = char.size()
-        # first transform to [batch *length, char_length, char_dim]
-        # then transpose to [batch * length, char_dim, char_length]
-        char = char.view(char_size[0] * char_size[1], char_size[2], char_size[3]).transpose(1, 2)
-        # put into cnn [batch*length, char_filters, char_length]
-        # then put into maxpooling [batch * length, char_filters]
-        char, _ = self.conv1d(char).max(dim=2)
-        # reshape to [batch, length, char_filters]
-        char = char.view(char_size[0], char_size[1], -1)
-
-        # concatenate word and char [batch, length, word_dim+char_filter]
-        input = F.tanh(torch.cat([word, char], dim=2))
-        # apply dropout
-        # [batch, length, dim] --> [batch, dim, length] --> [batch, length, dim]
-        input = self.dropout_in(input.transpose(1, 2)).transpose(1, 2)
-        # output from rnn [batch, length, hidden_size]
-        output, hn = self.rnn(input, mask, hx=hx)
-
-        output = self.dropout_rnn(output.transpose(1, 2)).transpose(1, 2)
-
-        if self.dense_tag is not None:
-            # [batch, length, tag_space]
-            output = F.tanh(self.dense_tag(output))
-
-        return output, hn, mask
-
-    def loss(self, input_word, input_char, target, mask=None, hx=None, leading_symbolic=0):
-        # [batch, length, num_labels]
-        output, _, _ = self.forward(input_word, input_char, mask=mask, hx=hx)
-        # [batch, length, num_labels]
-        output = self.dense_softmax(output)
-        # preds = [batch, length]
-        _, preds = torch.max(output[:, :, leading_symbolic:], dim=2)
-        preds += leading_symbolic
-
-        output_size = output.size()
-        # [batch * length, num_labels]
-        output_size = (output_size[0] * output_size[1], output_size[2])
-        output = output.view(output_size)
-        if mask is not None:
-            return self.nll_loss(self.logsoftmax(output) * mask.view(output_size[0], 1),
-                                 target.view(-1)) / mask.sum(), \
-               (torch.eq(preds, target).type_as(mask) * mask).sum(), preds
-        else:
-            num = output_size[0] * output_size[1]
-            return self.nll_loss(self.logsoftmax(output), target.view(-1)) / num, \
-                   (torch.eq(preds, target).type_as(output)).sum(), preds
