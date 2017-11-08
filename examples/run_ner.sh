08a9ea3e7b04c3ed9fd7a57a9d4e3aa3c1fd67ba
Max Ma <xuezhe.ma@gmail.com>
2017-11-08 01:04:25 -0500

    runs

--- examples/run_ner.sh
+++ examples/run_ner.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=2 python examples/NER.py --mode LSTM --num_epochs 200 --batch_size 16 --hidden_size 256 --num_filters 30 \
+CUDA_VISIBLE_DEVICES=2 python examples/NER.py --mode LSTM --num_epochs 200 --batch_size 16 --hidden_size 256 --num_filters 30 --tag_space 100 \
  --learning_rate 0.1 --decay_rate 0.05 --schedule 5 --gamma 0.0 --dropout std --p 0.5 \
  --embedding glove --embedding_dict "data/glove/glove.6B/glove.6B.100d.gz" \
  --train "data/conll2003/english/eng.train.bioes.conll" --dev "data/conll2003/english/eng.dev.bioes.conll" --test "data/conll2003/english/eng.test.bioes.conll"
--- examples/run_ner_ger.sh
+++ examples/run_ner_ger.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=2 python examples/NERCRF.py --mode LSTM --num_epochs 200 --batch_size 16 --hidden_size 256 --num_filters 30 \
- --learning_rate 0.015 --decay_rate 0.05 --schedule 1 --gamma 0.0 --dropout std --p 0.5 \
+CUDA_VISIBLE_DEVICES=2 python examples/NERCRF.py --mode LSTM --num_epochs 200 --batch_size 16 --hidden_size 256 --num_filters 30 --tag_space 50 \
+ --learning_rate 0.01 --decay_rate 0.05 --schedule 5 --gamma 0.0 --dropout std --p 0.5 \
  --embedding sskip --embedding_dict "data/sskip/sskip.ger.64.gz" \
- --train "data/conll2003/german/ger.train.bio.conll" --dev "data/conll2003/german/ger.dev.bio.conll" --test "data/conll2003/german/ger.test.bio.conll"
+ --train "data/conll2003/german/ger.train.bioes.conll" --dev "data/conll2003/german/ger.dev.bioes.conll" --test "data/conll2003/german/ger.test.bioes.conll"
