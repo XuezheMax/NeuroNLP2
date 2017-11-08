9f32285d7426a585caa5ad8ecb414322ffb859c1
Max Ma <xuezhe.ma@gmail.com>
2017-11-08 15:08:19 -0500

    runs

--- examples/run_ner_crf.sh
+++ examples/run_ner_crf.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=2 python examples/NERCRF.py --mode LSTM --num_epochs 200 --batch_size 16 --hidden_size 256 --num_filters 30 --tag_space 100 \
- --learning_rate 0.01 --decay_rate 0.05 --schedule 5 --gamma 0.0 --dropout std --p 0.5 --bigram \
+CUDA_VISIBLE_DEVICES=2 python examples/NERCRF.py --mode LSTM --num_epochs 200 --batch_size 16 --hidden_size 256 --num_filters 30 --tag_space 128 \
+ --learning_rate 0.015 --decay_rate 0.05 --schedule 2 --gamma 0.0 --dropout std --p 0.5 --bigram \
  --embedding glove --embedding_dict "data/glove/glove.6B/glove.6B.100d.gz" \
  --train "data/conll2003/english/eng.train.bioes.conll" --dev "data/conll2003/english/eng.dev.bioes.conll" --test "data/conll2003/english/eng.test.bioes.conll"
