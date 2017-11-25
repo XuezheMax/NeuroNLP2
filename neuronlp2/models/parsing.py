09b559e2f6b41d05dae2b040255425e7a8cf8068
Max Ma <xuezhe.ma@gmail.com>
2017-11-24 22:28:11 -0500

    debug

--- neuronlp2/models/parsing.py
+++ neuronlp2/models/parsing.py
@@ -789,8 +789,6 @@ class StackVarPtrNet(StackPtrNet):
         # [batch, length, dim] --> [batch, dim, length] --> [batch, length, dim]
         input = self.dropout_in(src_encoding.transpose(1, 2)).transpose(1, 2)
         # output from rnn [batch, length, hidden_size]
-        print(hx.size())
-        raw_input()
         output, hn = self.encoder(input, mask_e, hx=hx)
         # apply dropout
         output = self.dropout_rnn(output.transpose(1, 2)).transpose(1, 2)
@@ -813,6 +811,8 @@ class StackVarPtrNet(StackPtrNet):
         # [batch, length_decoder, dim] --> [batch, dim, length_decoder] --> [batch, length_decoder, dim]
         input = self.dropout_in(src_encoding.transpose(1, 2)).transpose(1, 2)
         # output from rnn [batch, length, hidden_size]
+        print(hx.size())
+        raw_input()
         output, hn = self.decoder(input, mask_d, hx=hx)
         # apply dropout
         output = self.dropout_rnn(output.transpose(1, 2)).transpose(1, 2)
