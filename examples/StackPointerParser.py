1917f63418c7ac53f86b3d45931924e8361c28a7
Max Ma <xuezhe.ma@gmail.com>
2017-12-03 23:19:18 -0500

    change beta2 to 0.9

--- examples/StackPointerParser.py
+++ examples/StackPointerParser.py
@@ -88,7 +88,7 @@ def main():
     learning_rate = args.learning_rate
     opt = args.opt
     momentum = 0.9
-    betas = (0.9, 0.99)
+    betas = (0.9, 0.9)
     rho = 0.9
     eps = 1e-6
     decay_rate = args.decay_rate
