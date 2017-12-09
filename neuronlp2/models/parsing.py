f64bab9ec653e3fab075fee6cdaa0a51a417e85a
Max Ma <xuezhe.ma@gmail.com>
2017-12-09 03:06:01 -0500

    delete print

--- neuronlp2/models/parsing.py
+++ neuronlp2/models/parsing.py
@@ -569,7 +569,6 @@ class StackPtrNet(nn.Module):
             beam_index = src_encoding.data.new(num_hyp).zero_().long()
             # [num_hyp]
             heads = torch.LongTensor([stacked_heads[i][-1] for i in range(num_hyp)]).type_as(children)
-            print(heads)
             # [num_hyp, 1, input_size]
             input = src_encoding[beam_index, heads].unsqueeze(1)
             # output [num_hyp, 1, hidden_size]
