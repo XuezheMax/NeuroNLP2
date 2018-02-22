d2286926680b3cf791a94401c672e5e3d85c116d
Max Ma <xuezhe.ma@gmail.com>
2018-02-22 14:36:17 -0500

    run

--- examples/run_stackPtrParser.sh
+++ examples/run_stackPtrParser.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 CUDA_VISIBLE_DEVICES=2 python examples/StackPointerParser.py --mode FastLSTM --num_epochs 1000 --batch_size 32 --decoder_input_size 256 --hidden_size 512 --encoder_layers 3 --decoder_layers 1 \
  --pos_dim 100 --char_dim 100 --num_filters 100 --arc_space 512 --type_space 128 \
- --opt adam --learning_rate 0.001 --decay_rate 0.75 --epsilon 1e-4 --schedule 10 --coverage 0.0 --gamma 0.0 --clip 5.0 \
+ --opt adam --learning_rate 0.001 --decay_rate 0.75 --epsilon 1e-4 --coverage 0.0 --gamma 0.0 --clip 5.0 \
+ --schedule 10 --double_schedule_decay 5 \
  --p_in 0.33 --p_out 0.33 --p_rnn 0.33 0.33 --unk_replace 0.5 --label_smooth 1.0 --pos --beam 1 --prior_order inside_out \
  --grandPar --sibling \
  --word_embedding sskip --word_path "data/sskip/sskip.eng.100.gz" --char_embedding random \
