604898da7a790f7155144cdac709d510199b7adf
Max Ma <xuezhe.ma@gmail.com>
2019-10-29 00:46:01 -0400

    init

--- neuronlp2/data/__init__.py
+++ neuronlp2/data/__init__.py
@@ -4,5 +4,5 @@ from neuronlp2.data.alphabet import Alphabet
 from neuronlp2.data.instance import *
 from neuronlp2.data.logger import get_logger
 from neuronlp2.data.writer import CoNLL03Writer, CoNLLXWriter
-from neuronlp2.data import conllx_data, conll03_data, conllx_stacked_data
 from neuronlp2.data.utils import get_batch, get_bucketed_batch, iterate_data
+from neuronlp2.data import conllx_data, conll03_data, conllx_stacked_data
