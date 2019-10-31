4e7e5fa4549ee4da68a2210e333e7b245e2d3fff
Max Ma <xuezhe.ma@gmail.com>
2019-10-30 23:11:39 -0400

    typo

--- experiments/run_pos_wsj.sh
+++ experiments/run_pos_wsj.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=0 python pos_tagging.py ---config configs/pos/wsj.json --num_epochs 400 --batch_size 32 \
+CUDA_VISIBLE_DEVICES=0 python pos_tagging.py --config configs/pos/wsj.json --num_epochs 400 --batch_size 32 \
  --loss_type sentence --optim sgd --learning_rate 0.01 --lr_decay 0.99999 --grad_clip 0.0 --warmup_steps 10 --weight_decay 0.0 --unk_replace 0.0 \
  --embedding glove --embedding_dict "data/glove/glove.6B/glove.6B.100d.gz" --model_path "models/pos/wsj" \
  --train "data/POS-penn/wsj/split1/wsj1.train.original" --dev "data/POS-penn/wsj/split1/wsj1.dev.original" --test "data/POS-penn/wsj/split1/wsj1.test.original" 
