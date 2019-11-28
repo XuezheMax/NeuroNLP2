b045a0d1322752ccfece9b376ca3c57ea9329934
Max Ma <xuezhe.ma@gmail.com>
2019-11-28 18:08:28 -0500

    mask stop

--- neuronlp2/models/parsing.py
+++ neuronlp2/models/parsing.py
@@ -990,10 +990,10 @@ class StackPtrNet(nn.Module):
             child_index_expand = child_index.unsqueeze(2).expand(batch, num_hyp, type_space)
             # [batch, num_hyp, num_labels]
             out_type = self.bilinear(type_h.gather(dim=1, index=base_index_expand), type_c.gather(dim=1, index=child_index_expand))
-            hyp_type_scores = F.log_softmax(out_type, dim=2).masked_fill_(mask_stop.view(batch, 1, 1), 0)
+            hyp_type_scores = F.log_softmax(out_type, dim=2)
             # compute the prediction of types [batch, num_hyp]
             hyp_type_scores, hyp_types = hyp_type_scores.max(dim=2)
-            hypothesis_scores = hypothesis_scores + hyp_type_scores
+            hypothesis_scores = hypothesis_scores + hyp_type_scores.masked_fill_(mask_stop.view(batch, 1), 0)
             types.scatter_(2, child_index.unsqueeze(2), torch.where(mask_leaf, org_types, hyp_types).unsqueeze(2))
 
             # hx [decoder_layer, batch * num_hyp, dec_dim]
