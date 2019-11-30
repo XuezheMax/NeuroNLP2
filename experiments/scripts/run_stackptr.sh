c111c5e8ca6be905e82dd90ca58b7eaf6fcd1944
Max Ma <xuezhe.ma@gmail.com>
2019-11-30 14:23:52 -0500

    hyp of stackptr

--- experiments/scripts/run_stackptr.sh
+++ experiments/scripts/run_stackptr.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=4 python -u parsing.py --mode train --config configs/parsing/stackptr.json --num_epochs 400 --batch_size 32 \
- --opt adam --learning_rate 0.001 --lr_decay 0.999995 --beta1 0.9 --beta2 0.9 --eps 1e-4 --grad_clip 5.0 \
+CUDA_VISIBLE_DEVICES=0 OMP_NUM_THREADS=4 python -u parsing.py --mode train --config configs/parsing/stackptr.json --num_epochs 500 --batch_size 32 \
+ --opt adam --learning_rate 0.001 --lr_decay 0.999997 --beta1 0.9 --beta2 0.9 --eps 1e-4 --grad_clip 5.0 \
  --loss_type token --warmup_steps 40 --reset 20 --weight_decay 0.0 --unk_replace 0.5 --beam 5 \
  --word_embedding sskip --word_path "data/sskip/sskip.eng.100.gz" --char_embedding random \
  --punctuation '.' '``' "''" ':' ',' \
