f0d0a6faac500789a5745f21dd7a95951bf6c777
Xuezhe Ma <max@Xuezhes-MacBook-Pro.local>
2019-12-04 02:32:56 -0500

    lr decay

--- neuronlp2/models/classifying.py
+++ neuronlp2/models/classifying.py
@@ -5,8 +5,6 @@ import torch
 import torch.nn as nn
 from torch.optim.adamw import AdamW
 
-from neuronlp2.optim import ExponentialScheduler
-
 
 def iterate_batch(data, labels, batch_size, shuffle=False):
     data_size = data.size(0)
@@ -25,14 +23,14 @@ def iterate_batch(data, labels, batch_size, shuffle=False):
 class Classifier:
     def __init__(self):
         self.core = nn.Module()
-        self.criterion = nn.CrossEntropyLoss(reduction='none')
+        self.criterion = nn.CrossEntropyLoss(reduction='mean')
 
     def clone_core(self):
         return nn.Module()
 
     def loss(self, x, y):
         out = self.core(x)
-        loss = self.criterion(out, y).mean()
+        loss = self.criterion(out, y)
         return loss
 
     def score(self, x, y, device):
@@ -48,15 +46,11 @@ class Classifier:
             corr += pred.eq(by).float().sum().item()
         return corr / total * 100
 
-    def fit(self, x_train, y_train, x_val=None, y_val=None, device=torch.device('cpu')):
-        optimizer = AdamW(self.core.parameters(), lr=1e-3, weight_decay=5e-4)
-        init_lr = 1e-7
-        scheduler = ExponentialScheduler(optimizer, 0.999995, 30, init_lr)
+    def fit(self, x_train, y_train, x_val, y_val, device=torch.device('cpu')):
+        lr = 5e-4
+        optimizer = AdamW(self.core.parameters(), lr=lr, weight_decay=5e-4)
         self.core = self.core.to(device)
-        if x_val is not None:
-            best_core = self.clone_core()
-        else:
-            best_core = None
+        best_core = self.clone_core()
         best_acc = 0.
         patient = 0
         for epoch in range(500):
@@ -67,22 +61,20 @@ class Classifier:
                 loss = self.loss(x, y)
                 loss.backward()
                 optimizer.step()
-                scheduler.step()
 
-            if x_val is not None:
-                with torch.no_grad():
-                    acc = self.score(x_val, y_val, device)
-                    # print('epoch: {}, acc: {:.2f}'.format(epoch, acc))
-                    if best_acc < acc:
-                        best_core.load_state_dict(self.core.state_dict())
-                        best_acc = acc
-                        patient = 0
-                    else:
-                        patient += 1
-            else:
-                best_acc = acc
+            with torch.no_grad():
+                acc = self.score(x_val, y_val, device)
+                # print('epoch: {}, acc: {:.2f}'.format(epoch, acc))
+                if best_acc < acc:
+                    best_core.load_state_dict(self.core.state_dict())
+                    best_acc = acc
+                    patient = 0
+                else:
+                    patient += 1
+                    lr *= 0.5
+                    optimizer = AdamW(self.core.parameters(), lr=lr, weight_decay=5e-4)
 
-            if patient > 4:
+            if patient > 9:
                 break
         if best_core is not None:
             self.core.load_state_dict(best_core.state_dict())
@@ -113,7 +105,7 @@ class MLPClassifier(Classifier):
         super(MLPClassifier, self).__init__()
         self.dropout = 0.4
         if hidden_features is None:
-            hidden_features = 4 * num_features
+            hidden_features = min(4 * num_features, 1024)
         self.core = nn.Sequential(
             nn.Dropout(p=self.dropout),
             nn.Linear(num_features, hidden_features),
